f8af94dc487270654d2fc5b3d9c8b91d9fcf307a
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-21 15:26:50 +0000

    Adding a Database

---
+++ app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//Database Connection (mongoDB)
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB")
+
+const itemsSchema = ({
+   name : String
+})
+
+const Item = mongoose.model("Item", itemsSchema)
+
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3]
+
+
+app.get("/", function(req, res) {
+
+  Item.find()
+  .then((foundItems) =>{
+    if(foundItems == 0){
+      Item.insertMany(defaultItems)
+        .then((defaultItems) => {
+          console.log("Successfully saved default items into DB")
+          console.log(defaultItems)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+    res.render("list", { listTitle: "Today", newListItems: foundItems });
+
+  })
+
+
+const day = date.getDate();
+
+ 
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+
+  const item = new Item ({
+    name: itemName
+  })
+
+  item.save()
+  res.redirect("/")
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
